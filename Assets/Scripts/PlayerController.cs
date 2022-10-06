0188b0f374771144e97d704209d8e417d3c6a50a
sadhaX18 <114281241+sadhaX18@users.noreply.github.com>
2022-10-06 15:44:40 +0100

    3. UI and Game End

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,24 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public Vector2 moveValue;
     public float speed;
     private int count;
+    private int numPickups = 5;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI winText;
 
     private void Start()
     {
         count = 0;
+        winText.text = "";
+        SetCountText();
     }
 
     private void FixedUpdate()
@@ -24,6 +33,15 @@ public class PlayerController : MonoBehaviour
         {
             other.gameObject.SetActive(false);
             count++;
+            SetCountText();
+        }
+    }
+    private void SetCountText()
+    {
+        scoreText.text = "Score: " + count.ToString();
+        if (count >= numPickups)
+        {
+            winText.text = "You Win";
         }
     }
 
